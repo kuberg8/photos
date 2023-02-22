3d206a28ce04b590b882bfb4cd85252f47304ff5
kuberg8 <kuberg8@gmail.com>
2023-02-22 18:10:39 +0500

    5c51ced

---
+++ _nuxt/static/1677071416/price/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/price", {data:[{_img:{}}],fetch:{},mutations:[]});
---
+++ _nuxt/static/1677071416/price/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fphotos\u002F_nuxt\u002Fstatic\u002F1677071416",layout:"default",error:a,state:{services:[]},serverRendered:true,routePath:"\u002Fprice",config:{_app:{basePath:"\u002Fphotos\u002F",assetsPath:"\u002Fphotos\u002F_nuxt\u002F",cdnURL:a}}}}(null));
